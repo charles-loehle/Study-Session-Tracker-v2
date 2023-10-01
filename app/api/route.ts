4c3ab381629ed8cd28760f59cf12070b352e4a0e
Charles Loehle <clguitar1@gmail.com>
2023-10-01 08:09:53 -0400

    deleted api folder

--- app/api/route.ts
+++
@@ -1,7 +0,0 @@
-// import { NextResponse } from 'next/server';
-// import supabase from '../config/supabaseClient';
-
-// export async function GET(request: Request) {
-// 	const { data } = await supabase.from('study-sessions').select();
-// 	return NextResponse.json(data);
-// }
